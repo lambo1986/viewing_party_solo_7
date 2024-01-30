9725a99358d49546fdb83d0d580074ee0e0235de
Nathan Lambertson <lambertones@gmail.com>
2024-01-30 14:55:18 -0600

    Clean

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -6,17 +6,14 @@ RSpec.describe "user's discover page (/users/:id/discover)", type: :feature do# 
       visit "/users/1/discover"
 
       expect(page).to have_button("Top 20 Movies")
-
       click_button("Top 20 Movies")
-
       expect(current_path).to eq("/users/1/movies")# movie results page (will come in US-2)
 
       visit "/users/1/discover"
 
       fill_in "search", with: "Big"
       click_button "Search"
-
-      expect(current_path).to eq("users/1/movies")# movie results page (will come in US-2)
+      expect(current_path).to eq("/users/1/movies")# movie results page (will come in US-2)
     end
   end
 end
