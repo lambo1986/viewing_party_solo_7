a603a5d8afbd46c44b23494f3447362ec05974e3
Nathan Lambertson <lambertones@gmail.com>
2024-01-30 21:47:36 -0600

    Add route for movie controller show... fingers crossed

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new', as: 'register_user'
 
   resources :users, only: [:show, :create] do
-    get '/discover', to: 'users#discover', on: :member#not sure if this is where I want to go with this route
+    get '/discover', to: 'users#discover', on: :member# not sure if this is where I want to go with this route
     get '/movies', to: 'users#movies', on: :member
+    resources :movies, only: [:show], controller: 'movies'# I might regret this later (maybe user_movies controller?)
   end
 end
