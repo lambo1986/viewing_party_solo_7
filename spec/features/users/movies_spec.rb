8f080f714f4b62b576047aaf7563cc60ed50751c
Nathan Lambertson <lambertones@gmail.com>
2024-01-30 20:10:37 -0600

    Update test for US-2

--- spec/features/users/movies_spec.rb
+++ spec/features/users/movies_spec.rb
@@ -6,12 +6,19 @@ RSpec.describe "movies page for a user", type: :feature do# US-2
 
     expect(page).to have_button("Top 20 Movies")
     click_button("Top 20 Movies")
+
     expect(current_path).to eq("/users/1/movies")
+    expect(page).to have_content("Top 20 Movies")
+    expect(page).to have_content("Shawshank Redemption")
 
     visit "/users/1/discover"
 
     fill_in "search", with: "Big"
+  
     click_button "Search"
+
     expect(current_path).to eq("/users/1/movies")
+    expect(page).to have_content("Searched Movie:")
+    expect(page).to have_content("Big")
   end
 end
