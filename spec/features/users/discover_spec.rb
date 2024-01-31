e659175175b0fa9748195f562f1198b6cee1fee1
Nathan Lambertson <lambertones@gmail.com>
2024-01-30 21:47:10 -0600

    Made user and dynamic id

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -3,17 +3,18 @@ require "rails_helper"#why this over spec_helper?
 RSpec.describe "user's discover page (/users/:id/discover)", type: :feature do# US-1
   describe "discover and search" do
     it "allows users to search for a movie by title or discover top rated movies" do
-      visit "/users/1/discover"
+      user = User.create!(name: 'Sam', email: 'sam@email.com')# needed this line to create a user to test against
+      visit "/users/#{user.id}/discover"
 
       expect(page).to have_button("Top 20 Movies")
       click_button("Top 20 Movies")
-      expect(current_path).to eq("/users/1/movies")# movie results page (will come in US-2)
+      expect(current_path).to eq("/users/#{user.id}/movies")# movie results page (will come in US-2)
 
-      visit "/users/1/discover"
+      visit "/users/#{user.id}/discover"
 
       fill_in "search", with: "Big"
       click_button "Search"
-      expect(current_path).to eq("/users/1/movies")# movie results page (will come in US-2)
+      expect(current_path).to eq("/users/#{user.id}/movies")# movie results page (will come in US-2)
     end
   end
 end
