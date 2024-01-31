881bfbb6cedfefee991c0bfec8d0ba8bda5528e4
Nathan Lambertson <lambertones@gmail.com>
2024-01-31 16:59:18 -0600

    Add some mo to the US-3 test

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Movie's detail page", type: :feature do
+RSpec.describe "Movie's detail page", type: :feature do# US-3
   it "has a button to go back to the discover page, a button to create a viewing party, and many movie details", :vcr do
     user = User.create!(name: 'Sam', email: 'sam@email.com')
 
@@ -16,7 +16,7 @@ RSpec.describe "Movie's detail page", type: :feature do
 
     click_link("Big")
 
-    expect(current_path).to eq "/users/#{user.id}/movies/2280"
+    expect(current_path).to eq "/users/#{user.id}/movies/2280"# we have arrived
     expect(page).to have_button("Return to Discover")
     expect(page).to have_content("Movie Information")
     expect(page).to have_button("Create a Viewing Party")
@@ -24,5 +24,15 @@ RSpec.describe "Movie's detail page", type: :feature do
     expect(page).to have_content("Vote Average: 7.163")# vote_average
     expect(page).to have_content("Vote Count: 3384")# vote_count
     expect(page).to have_content("Summary: When a young boy makes a wish")# overview
+    expect(page).to have_content("Genre:")# 14, 18, 35, 10749, 10751
+    expect(page).to have_content("Runtime:")# 1hr 44min
+    expect(page).to have_content("Cast Members:")# first 10
+    expect(page).to have_content("Reviews Count:")
+    expect(page).to have_content("Review Author:")
+    expect(page).to have_content("Review Content:")
+
+    click_button("Create a Viewing Party")
+
+    expect(current_path).to eq "/users/#{user.id}/movies/#{2280}/viewing_party/new"
   end
 end
