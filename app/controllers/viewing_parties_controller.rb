4fc6f141ba542af334699471c31c4f40c7ad0748
Nathan Lambertson <lambertones@gmail.com>
2024-02-01 12:30:57 -0600

    Trying to make sure the party time is equal to or greater than the movie length

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -7,14 +7,16 @@ class ViewingPartiesController < ApplicationController
 
   def create
     @user = current_user
-    @viewing_party = ViewingParty.new(viewing_party_params.except(:guest_ids))
+    @movie = current_movie
+    @viewing_party = ViewingParty.new(viewing_party_params.except(:guest_ids))# guest_ids not needed for this, but needed for adding guests to viewing_party
+    @viewing_party.movie_runtime = @movie[:runtime]# 104 is movie duration (and 135 is the party duration of the party)
 
-    if @viewing_party.save
+    if @viewing_party.save && (@viewing_party.movie_runtime.to_i <= @viewing_party.duration.to_i)# hope this is enough
       UserParty.create!(user_id: @user.id, viewing_party_id: @viewing_party.id, host: true)# sets the current user as host
       @viewing_party.users << party_guests# adds the guests to the viewing party
       redirect_to user_path(current_user), notice: 'Viewing party was successfully created.'
     else
-      render :new# test for this?
+      redirect_to "/users/#{@user.id}/movies/#{@movie[:id]}/viewing_party/new", alert: 'party duration must be greater than movie duration'
     end
   end
 end
