e4f32502d25adbdeffb7d1d831fe6a06aa5dcd39
Nathan Lambertson <lambertones@gmail.com>
2024-01-31 19:58:23 -0600

    Add stuff I need to the controller

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,5 +1,7 @@
 class ViewingPartiesController < ApplicationController
   def new
-    
+    @movie = MovieService.search_by_id(params[:movie_id])
+    @user = User.find_by_id(params[:user_id])
+    @viewing_party = ViewingParty.new
   end
 end
