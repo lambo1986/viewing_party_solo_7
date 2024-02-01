c1271fea02113944d3078993ad28fa2e56d6b925
Nathan Lambertson <lambertones@gmail.com>
2024-02-01 16:47:16 -0600

    Add hand-rolled route for show page... after much struggle, worked best

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get '/movies', to: 'users#movies', on: :member
     resources :movies, only: [:show] do# I might regret this later (need user_movies controller?)
       resource :viewing_party, only: [:new, :create]
+      get '/viewing_party/:id', to: 'viewing_parties#show'
     end
   end
 end
