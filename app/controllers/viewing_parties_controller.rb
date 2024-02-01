aa553ed1237ae478f89fb076ff3bb84973b013b0
Nathan Lambertson <lambertones@gmail.com>
2024-02-01 09:52:47 -0600

    Method for creating movie film party

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,21 +1,35 @@
 class ViewingPartiesController < ApplicationController
   def new
-    @movie = MovieService.search_by_id(params[:movie_id])
-    @user = User.find_by_id(params[:user_id])
+    @movie = current_movie
+    @user = current_user
     @viewing_party = ViewingParty.new
   end
 
   def create
-    redirect_to user_path(current_user)
+    @user = current_user
+    @viewing_party = ViewingParty.new(viewing_party_params.except(:guest_ids))
+
+    if @viewing_party.save
+      UserParty.create!(user_id: @user.id, viewing_party_id: @viewing_party.id, host: true)# sets the current user as host
+      guests = User.where(id: params[:viewing_party][:guest_ids].reject(&:blank?))# nested ids, and skip the blank ones
+      @viewing_party.users << guests# adds the guests to the viewing party 
+      redirect_to user_path(current_user), notice: 'Viewing party was successfully created.'
+    else
+      render :new# test for this?
+    end
   end
 end
 
 private
 
 def viewing_party_params
-  params.require(:viewing_party).permit(:duration, :date, :start_time, :guest_ids)
+  params.require(:viewing_party).permit(:duration, :date, :start_time, guest_ids: [])
 end
 
 def current_user
-  current_user = User.find_by_id(params[:user_id])
+  User.find_by_id(params[:user_id])
+end
+
+def current_movie
+  MovieService.search_by_id(params[:movie_id])
 end
