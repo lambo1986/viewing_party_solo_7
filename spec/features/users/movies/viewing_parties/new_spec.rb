2673e74f5bbfcec65d44fd66bb19efb74c747a7a
Nathan Lambertson <lambertones@gmail.com>
2024-01-31 20:39:57 -0600

    Add comment

--- spec/features/users/movies/viewing_parties/new_spec.rb
+++ spec/features/users/movies/viewing_parties/new_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "New Viewing Party Page", type: :feature, vcr: :true do
+RSpec.describe "New Viewing Party Page", type: :feature, vcr: :true do# US-4
   it "Shows the movie title that the party is for" do
     user = User.create!(name: 'Sam', email: 'sam@email.com')
     user1 = User.create!(name: 'Jams', email: 'jams@email.com')
@@ -20,7 +20,7 @@ RSpec.describe "New Viewing Party Page", type: :feature, vcr: :true do
     expect(page).to have_button("Create a Viewing Party")
     expect(page).to have_content("Title: Big")# title
 
-    click_button("Create a Viewing Party")#begin the test
+    click_button("Create a Viewing Party")#begin the test.. part 1 (new party)
 
     expect(current_path).to eq "/users/#{user.id}/movies/#{2280}/viewing_party/new"
     expect(page).to have_content("Create a Party for Big")
@@ -32,5 +32,18 @@ RSpec.describe "New Viewing Party Page", type: :feature, vcr: :true do
     expect(page).to have_field(user2.name, type: 'checkbox')
     expect(page).to have_field(user3.name, type: 'checkbox')
     expect(page).to have_button("Create Party")
+
+    fill_in "Duration of Party", with: "135"
+    fill_in "Date", with: "03-27-24"
+    fill_in "Start Time", with: "20:00"
+    check(user1.name)
+    check(user2.name)
+    check(user3.name)
+    click_button("Create Party")
+
+    expect(current_path).to eq "/users/#{user.id}"#begin part 2 (user dashboard)
+    expect(page).to have_content("Sam")
+    expect(page).to have_content("Upcoming Viewing Party:")
+    
   end
 end
