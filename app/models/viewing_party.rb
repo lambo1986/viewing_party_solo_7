6ec05974d24b53402b377f15d8f2e0fc6158fe5a
Nathan Lambertson <lambertones@gmail.com>
2024-02-01 09:52:21 -0600

    Create validation method perhaps?

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,8 +1,16 @@
 class ViewingParty < ApplicationRecord
+   validate :duration_longer_than_movie# brainstorming
+
    has_many :user_parties
    has_many :users, through: :user_parties
 
    def find_host
       users.where("user_parties.host = true").first
    end
+end
+
+private
+
+def duration_longer_than_movie# brainstorming
+
 end
