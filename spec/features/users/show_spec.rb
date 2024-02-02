bb8357ff2bbdbe23fb798ad638e1467370f53fb2
Nathan Lambertson <lambertones@gmail.com>
2024-02-02 11:17:19 -0600

    Add quick test and button for finding where to watch movie

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe "user dashboard", type: :feature, vcr: true do
     expect(page).to have_content("Title: The Godfather")
   end
 
-  it "has a link to discover movies" do# added for smooth rails server experience (can get anywhere from root now)
+  it "has a button to discover movies" do# added for smooth rails server experience (can get anywhere from root now)
     user = User.create!(name: 'Sam', email: 'sam@email.com')
 
     visit "/users/#{user.id}"
@@ -41,5 +41,25 @@ RSpec.describe "user dashboard", type: :feature, vcr: true do
     click_button "Discover Movies"
 
     expect(current_path).to eq("/users/#{user.id}/discover")
+  end
+
+  it "has a button to see where to rent or buy movie" do
+    user = User.create!(name: 'Sam', email: 'sam@email.com')
+    user1 = User.create!(name: 'Jams', email: 'jams@email.com')
+    user2 = User.create!(name: 'Ari', email: 'ari@email.com')
+    user3 = User.create!(name: 'Tok', email: 'tok@email.com')
+    movie1 = MovieService.search_by_id(2280)#big
+    movie2 = MovieService.search_by_id(238)#godfather
+    viewing_party1 = ViewingParty.create!(duration: 135, date: "2020-01-01", start_time: "12:00", movie_id: 2280)#sam
+    viewing_party2 = ViewingParty.create!(duration: 235, date: "2021-02-03", start_time: "11:30", movie_id: 238)#jams
+    user_party1 = UserParty.create!(user: user, viewing_party: viewing_party1, host: true)# sam host
+    user_party2 = UserParty.create!(user: user1, viewing_party: viewing_party2, host: true)# jams host
+
+    viewing_party1.users << [user1, user2, user3]# sam invites Jams, Ari, Tok
+    viewing_party2.users << [user, user2, user3]# jams invites sam, Ari, Tok
+
+    visit "/users/#{user.id}"
+
+    expect(page).to have_button("Where to Watch")
   end
 end
