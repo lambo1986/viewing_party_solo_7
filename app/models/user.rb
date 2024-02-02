3ed9f21d94446c9ec0034420391c3476fb9add18
Nathan Lambertson <lambertones@gmail.com>
2024-02-01 23:25:30 -0600

    User model methods for finding hosted or unhosted parties

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
 
    has_many :user_parties
    has_many :viewing_parties, through: :user_parties
+
+   def hosted_viewing_parties
+      ViewingParty.joins(:user_parties).where(user_parties: { user_id: id, host: true }).distinct
+   end
+
+   def viewing_parties_attended
+      ViewingParty.joins(:user_parties).where.not(user_parties: { user_id: id, host: true }).distinct
+   end
 end
