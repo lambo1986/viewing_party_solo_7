2426089b7aa1084f6b6e6c589f72af1f369ae814
Nathan Lambertson <lambertones@gmail.com>
2024-02-03 15:24:31 -0600

    Remove comment

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -4,22 +4,16 @@ class ViewingParty < ApplicationRecord
    has_many :user_parties
    has_many :users, through: :user_parties
 
-   validate :duration_longer_than_movie# brainstorming
+   validate :duration_longer_than_movie
 
    def find_host
       users.where("user_parties.host = true").first
    end
-
-   # def add_guests(user_ids)#US-7 tried this
-   #    user_ids.each do |user_id|
-   #       UserParty.create!(viewing_party: self, user_id: user_id, host: false)
-   #    end
-   # end
 end
 
 private
 
-def duration_longer_than_movie# brainstorming
+def duration_longer_than_movie
    if movie_runtime.present? && duration < movie_runtime.to_i
       errors.add(:duration, "party duration must be greater than movie duration")
    end
