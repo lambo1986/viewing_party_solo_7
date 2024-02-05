9fa732a917a82541d6566b3cbce9319f709f784f
Nathan Lambertson <lambertones@gmail.com>
2024-02-04 20:40:11 -0600

    Refactor service class a little more

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -31,7 +31,7 @@ class MovieService
       data[:genres]
     end
 
-    def genres_of_movie(movie_id)#US-3
+    def genres_of_movie(movie_id)# US-3
       movie = search_by_id(movie_id)
       movie[:genres].map { |genre| genre[:name] }
     end
@@ -42,41 +42,26 @@ class MovieService
     end
 
     def cast(movie_id)# US-3
-      response = conn.get("/3/movie/#{movie_id}/credits") do |req|# input movie id
-        req.params['api_key'] = API_KEY
-      end
-      data = JSON.parse(response.body, symbolize_names: true)
+      data = get_url("/3/movie/#{movie_id}/credits")
       data[:cast].take(10)
     end
 
     def reviews(movie_id)# US-3
-      response = conn.get("/3/movie/#{movie_id}/reviews") do |req|# input movie id and user key
-        req.params['api_key'] = API_KEY
-      end
-      JSON.parse(response.body, symbolize_names: true)
+      get_url("/3/movie/#{movie_id}/reviews")
     end
 
     def providers_rent(movie_id)# US-5
-      response = conn.get("/3/movie/#{movie_id}/watch/providers") do |req|
-        req.params['api_key'] = API_KEY
-      end
-      data = JSON.parse(response.body, symbolize_names: true)
+      data = get_url("/3/movie/#{movie_id}/watch/providers")
       data[:results][:US][:buy].map { |service| service[:logo_path] }
     end
 
     def providers_buy(movie_id)# US-5
-      response = conn.get("/3/movie/#{movie_id}/watch/providers") do |req|
-        req.params['api_key'] = API_KEY
-      end
-      data = JSON.parse(response.body, symbolize_names: true)
+      data = get_url("/3/movie/#{movie_id}/watch/providers")
       data[:results][:US][:rent].map { |service| service[:logo_path] }
     end
 
     def similar(movie_id)# US-6
-      response = conn.get("/3/movie/#{movie_id}/similar") do |req|
-        req.params['api_key'] = API_KEY
-      end
-      data = JSON.parse(response.body, symbolize_names: true)
+      data = get_url("/3/movie/#{movie_id}/similar")
       data[:results]
     end
 
