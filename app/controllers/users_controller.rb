1f93bbbc47fad155b4700977a27bed9c50be6db4
Nathan Lambertson <lambertones@gmail.com>
2024-02-12 14:47:30 -0600

    Alter Params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
 private
 
-   def user_params
-      params.require(:user).permit(:name, :email)
+def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
    end
 end
