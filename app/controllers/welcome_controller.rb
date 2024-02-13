cb41dca05bfc94197e847643342d8e621506f2b9
Nathan Lambertson <lambertones@gmail.com>
2024-02-13 08:56:31 -0600

    Add a cookie to the welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @users = User.all
+    unless cookies.encrypted[:greeting]
+      cookies.encrypted[:greeting] = 'Howdy!'
+    end
   end
 end
