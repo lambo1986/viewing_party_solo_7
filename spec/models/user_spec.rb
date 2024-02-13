e5a82d562b0e00a7276b2902695feccadb8784a0
Nathan Lambertson <lambertones@gmail.com>
2024-02-12 19:07:37 -0600

    Alter user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
-    it { should validate_presence_of :password }
+    it { should validate_presence_of(:password_digest)}
     it { should have_secure_password }
     it { should allow_value('something@something.something').for(:email) }
     it { should_not allow_value('something somthing@something.something').for(:email) }
