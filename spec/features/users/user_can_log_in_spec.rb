5ab741c5f0b194ad08f08204f47632dd67c4d6d9
Nathan Lambertson <lambertones@gmail.com>
2024-02-12 19:07:52 -0600

    Write a test for login

---
+++ spec/features/users/user_can_log_in_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+RSpec.describe "Loggin in", type: :feature do
+  it "happy: allows a user to log in with credentials" do
+    user = User.create!(name: "Bap", email: "doo@doo.com", password: "getit3", password_confirmation: "getit3")
+
+    visit root_path
+
+    expect(page).to have_link "Log In"
+
+    click_link "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "Username:", with: user.name
+    fill_in "Password:", with: user.password
+    fill_in "Confirm Password:", with: user.password
+
+    click_button "Log In"
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_content "Hello, Bap!"
+  end
+
+  it "sad: does not allow a user to log in with invalid credentials" do
+    user = User.create!(name: "Bap", email: "doo@doo.com", password: "getit3", password_confirmation: "getit3")
+
+    visit root_path
+
+    expect(page).to have_link "Log In"
+
+    click_link "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "Username:", with: user.name
+    fill_in "Password:", with: user.password
+    fill_in "Confirm Password:", with: "doodah?"
+
+    click_button "Log In"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content "Invalid Username or Password"
+  end
+end
